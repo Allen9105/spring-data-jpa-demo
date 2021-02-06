9e1113cd18dfe784aed9e5d11565e198391f85d5
suncheng <sun_cheng@126.com>
2021-02-06 14:10:20 +0800

    :egg: 初始化 03.query-result-processing 模块

---
+++ 03.query-result-processing/src/main/java/com/suncheng/query/result/processing/QueryResultProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.suncheng.query.result.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryResultProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryResultProcessingApplication.class, args);
+    }
+
+}
